446b5fd744142dfc3c0e4f1ca89d93399ab6d780
dong <donghyunwon610@gmail.com>
2020-12-15 06:02:06 +0000

    renaming and organizing

--- run.py
+++ run.py
@@ -2,11 +2,9 @@
 #%%
 #run callie_scripts with run.py
 from scrapers.ipoScrape import start_scrape
-from callie_scripts.make_options_req import MakeRequest 
+from option_scripts.get_options_from_tda import MakeRequest 
 
 start_scrape()
 make_request = MakeRequest
 #%%
 #from callie_scripts import get_earnings_date
-
-
