625330b6e0de7592d0ed0af901fcd2494ec82b4e
dong <dong@gmail.com>
2021-01-01 17:35:30 -0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import React from "react"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Table from "./components/table/ApiTables";
-import Navbar from "./components/navbar/Navbar";
-import Home from "./components/home/Home"
+import Table from './components/table/ApiTables';
+import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home';
 
 function App() {
-  return (
-    <Router>
-      <React.Fragment>
-        <div className="App">
-          <Navbar />
-          <Switch>
-            <Route exact path="/tables" component={Table} />
-            <Route exact path="/" component={Table} />
-          </Switch>
-        </div>
-      </React.Fragment>
-    </Router>
-  );
+	return (
+		<Router>
+			<React.Fragment>
+				<Navbar />
+				<h1 className="display-6 text-center">Quickly find calendar ratios!</h1>
+				<h1 className="display-6 text-center">The golden ratio is calculated by short / long leg</h1>
+				<Switch>
+					<Route exact path="/tables" component={Table} />
+					<Route exact path="/" component={Table} />
+				</Switch>
+			</React.Fragment>
+		</Router>
+	);
 }
-
 export default App;
