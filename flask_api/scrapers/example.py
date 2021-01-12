3ca42b2958c6d47f98ad7b4490920626b4546f4f
dong <dong@gmail.com>
2021-01-12 04:34:35 -0500

    earnings added to api and bot

--- flask_api/scrapers/example.py
+++ flask_api/scrapers/example.py
@@ -3,4 +3,4 @@ import requests, pandas as pd, bs4 as bs
 from headers_generator  import make_headers
 ## for future uses
 URL = ""
-res = requests.get(URL, headers=make_headers()).content
+http://192.168.2.10:8081/res = requests.get(URL, headers=make_headers()).content
