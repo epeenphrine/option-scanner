a63918679e2fff0dea71fef4082bfc3c7a4dea50
dong <dong@gmail.com>
2021-02-24 08:38:30 -0500

    halts faster update / function rename

--- bot/bot.py
+++ bot/bot.py
@@ -243,10 +243,10 @@ async def earnings(ctx,*arg):
 #         ticker = arg[1]
          
 @tasks.loop(seconds=1)
-async def get_tweets_60s():
+async def get_tweets_1s():
     main_chat_id = 492405515931090966 
     tweets_chat_id = 801541146668564521
-    print('in get_tweets_60s')
+    print('in get_tweets_1s')
     main_chat = client.get_channel(main_chat_id)
     tweets_chat = client.get_channel(tweets_chat_id)
     new_tweet_urls = get_tweet_urls()
@@ -265,11 +265,11 @@ async def get_tweets_60s():
     else:
         print('no message skiping await')
 
-@tasks.loop(seconds=30)
-async def get_halts_30s():
+@tasks.loop(seconds=1)
+async def get_halts_1s():
     main_chat_id = 492405515931090966 
     halt_chat_id = 808544039850344488 
-    print('in get_halts_30s')
+    print('in get_halts_1s')
     main_chat = client.get_channel(main_chat_id)
     halt_chat = client.get_channel(halt_chat_id)
     new_halts = get_halts()
@@ -288,21 +288,22 @@ async def get_halts_30s():
             await  halt_chat.send(resume_message)
         # await tweets_chat.send(url)
         messages.append(message)
-    print('awaiting 28')
-    await asyncio.sleep(28)
-    for message in messages:
-        print(f'deleting : {message}')
-        await discord.Message.delete(message)
+    if messages:
+        print('awaiting 60')
+        await asyncio.sleep(60)
+        for message in messages:
+            print(f'deleting : {message}')
+            await discord.Message.delete(message)
 
-@get_tweets_60s.before_loop
+@get_tweets_1s.before_loop
 async def before():
     await client.wait_until_ready()
     print("Finished waiting")
 
-@get_halts_30s.before_loop
+@get_halts_1s.before_loop
 async def before():
     await client.wait_until_ready()
     print('finished awaiting')
-get_tweets_60s.start()
-get_halts_30s.start()
+get_tweets_1s.start()
+get_halts_1s.start()
 client.run(prod)
